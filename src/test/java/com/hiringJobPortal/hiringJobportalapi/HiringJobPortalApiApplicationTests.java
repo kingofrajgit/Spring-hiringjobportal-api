3e19d55303b70f675f9da10c1ba71a624dd39a66
kingofrajgit <kingofraj.7092@gmail.com>
2022-04-27 10:05:24 +0530

    first commit

---
+++ src/test/java/com/hiringJobPortal/hiringJobportalapi/HiringJobPortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiringJobPortal.hiringJobportalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiringJobPortalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
