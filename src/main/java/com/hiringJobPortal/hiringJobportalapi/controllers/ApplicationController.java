231508e5154e2f50dd3f2b4522f8d28b03597b9c
kingofrajgit <kingofraj.7092@gmail.com>
2022-05-03 15:52:13 +0530

    sonarcloud

--- src/main/java/com/hiringJobPortal/hiringJobportalapi/controllers/ApplicationController.java
+++ src/main/java/com/hiringJobPortal/hiringJobportalapi/controllers/ApplicationController.java
@@ -1,14 +1,13 @@
 package com.hiringJobPortal.hiringJobportalapi.controllers;
 
-import java.sql.SQLException;
-import java.time.LocalDate;
+
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.web.bind.annotation.RestController;
